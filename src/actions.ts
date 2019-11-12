ce29785aadce14067f50cebc15c1ca6492781ade
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-11-12 17:46:19 +0100

    thoughts

--- src/actions.ts
+++ src/actions.ts
@@ -1,6 +1,14 @@
 import {Cell, PlayerUnit} from "./model";
 import {Adventure} from "./model/Adventure";
 
+/**
+ * business logic should be tied to the model
+ * All actions here should just be callback creators
+ * to delay execution of that business logic and bind arguments
+ *
+ * TODO: figure out, how to create Callables that could be checked with instanceof, or add type property, to identify a callback as an action
+ */
+
 export function selectAction(adventure: Adventure, unit: PlayerUnit) {
     return () => adventure.activeUnit = unit;
 }
